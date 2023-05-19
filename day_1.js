79b244e9a02b61c6f972395a98a475949531dfbc
Mir Nushrat <nusrat.lilac@gmail.com>
2023-05-19 18:08:50 +0600

    initial commit with syntex, variable, operation

---
+++ day_1.js
@@ -0,0 +1,24 @@
+
+//from playlist 1 (50/130)
+
+
+// //JavaScript Syntax
+// let y=10+5;
+// console.log(y);
+//
+// //variable in javascript
+// let x=5;//let can not be redeclared
+// var y =6;
+// var y=9;// var variable can redeclared
+// var total = x+y;
+// console.log(total);
+
+//javascript operations
+let text1 = "mir";
+let text2 = "nushrat";
+let add = text1 + " " + text2;
+console.log(add);
+console.log(typeof text1);//data type of the value
+
+
+
