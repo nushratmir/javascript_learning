e3de354c8b82cb14953c4df82c9c6ea30f040489
Mir Nushrat <nusrat.lilac@gmail.com>
2023-05-20 02:18:07 +0600

    array

--- day_1.js
+++ day_1.js
@@ -21,40 +21,53 @@
 // console.log(typeof text1);//data type of the value
 
 //javascript function
-function convertCelsius(fahrenheit) {
-    return (5/9) * (fahrenheit-32); //calculate and return the value
+// function convertCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32); //calculate and return the value
+// }
+//
+// let value = convertCelsius(77);
+// console.log(value);
+//
+//
+// //javascript object
+// const house = {type:"duplex", model:"Aw1q", color:"white"};
+// console.log(house.model);
+//
+// //javascript string method
+// const person = { //object declare
+//     firstName:"Jahan",
+//     lastName:"Mir",
+//     age: 40,
+//     eyeColor:"blue"};
+//
+// let humanBeing = "tara"; //its a string
+// console.log(humanBeing.length); //when we are using method it become object
+//  let x= humanBeing.slice(1,3);
+//  console.log(x);
+//
+//
+//  //template literal
+// let str1 ="mir";
+// let str2 ="miru";
+// let str3 ="mira";
+//
+// let completeStr = str1 + " "+str2 + " "+str3 ;
+// let finalStr = `${str1} ${str2} ${str3}`;
+//
+// console.log(finalStr);
+
+//number methods
+let x = 123;
+let newN = x.toString();
+console.log(newN) ;
+
+//array
+const member=["rita","bina","tina"] ;
+for(i=0; i<member.length; i++){
+console.log(member[i]);
 }
 
-let value = convertCelsius(77);
-console.log(value);
 
-
-//javascript object
-const house = {type:"duplex", model:"Aw1q", color:"white"};
-console.log(house.model);
-
-//javascript string method
-const person = { //object declare
-    firstName:"Jahan",
-    lastName:"Mir",
-    age: 40,
-    eyeColor:"blue"};
-
-let humanBeing = "tara"; //its a string
-console.log(humanBeing.length); //when we are using method it become object
- let x= humanBeing.slice(1,3);
- console.log(x);
-
-
- //template literal
-let str1 ="mir";
-let str2 ="miru";
-let str3 ="mira";
-
-let completeStr = str1 + " "+str2 + " "+str3 ;
-let finalStr = `${str1} ${str2} ${str3}`;
-
-console.log(finalStr);
 
 
 
